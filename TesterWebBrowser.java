3a95b3ce06ec5024f9db21c828bdd0df7d320d08
yenalbert <51310108+yenalbert@users.noreply.github.com>
2019-06-02 23:55:15 -0700

    Update and rename TesterWebBrowser to TesterWebBrowser.java

--- TesterWebBrowser.java
+++ TesterWebBrowser.java
@@ -6,26 +6,24 @@ public class TesterWebBrowser
 {
 
 	public static void main(String[] args) 
-  {
+	{
   
 		Random randomBrowserHistory = new Random();
 		
 		//Creation of a new BoundedStack, type: String, for the Browser History;
-    BoundedStack<String> browserHistory = new BoundedStack<String>(Long.toString(randomBrowserHistory.nextLong()));
+		BoundedStack<String> browserHistory = new BoundedStack<String>(Long.toString(randomBrowserHistory.nextLong()));
         
-    //This will push 51 IP addresses, randomly generated Long-values;
+		//This will push 51 IP addresses, randomly generated Long-values;
 		for (int i=1; i != 53; i++) 
 		{
 			browserHistory.push(Long.toString(randomBrowserHistory.nextLong()));
 		}
 		
-        //After the 50th loop, this cycle will print null for the 51st IP address;
+		//After the 50th loop, this cycle will print null for the 51st IP address;
 		for (int i=1; i != 52; i++) 
 		{
 			System.out.println("IP Address #" + i + ": " + browserHistory.pop());
 		}
-		
-		
+			
 	}
-
 }
