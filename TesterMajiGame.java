9e75aeafeb595e3b16e49baf8eb7b9eef27d22c4
yenalbert <51310108+yenalbert@users.noreply.github.com>
2019-06-02 23:59:41 -0700

    Update TesterMajiGame.java

--- TesterMajiGame.java
+++ TesterMajiGame.java
@@ -5,25 +5,25 @@ import java.util.Random;
  */
 public class TesterMajiGame 
 {
-	  public static void main(String[] args) 
-    {
-		    //This is the Main Tester class for the Maji Game's Requirements;
-		    Random randomMajiSpells = new Random();
+	public static void main(String[] args) 
+	{
+		//This is the Main Tester class for the Maji Game's Requirements;
+		Random randomMajiSpells = new Random();
 		
-		    //Creation of a new BoundedStack, type: Double, for the Maji Game;
-        BoundedStack<Double> majiGame = new BoundedStack<Double>(randomMajiSpells.nextDouble());
+		//Creation of a new BoundedStack, type: Double, for the Maji Game;
+        	BoundedStack<Double> majiGame = new BoundedStack<Double>(randomMajiSpells.nextDouble());
         
-        //This will push 51 spells, with randomly varying Double-values;
-        for (int i=1; i != 53; i++) 
-        {
-        	  majiGame.push(randomMajiSpells.nextDouble());
-        }
+       		//This will push 51 spells, with randomly varying Double-values;
+        	for (int i=1; i != 53; i++) 
+		{
+			majiGame.push(randomMajiSpells.nextDouble());
+        	}
         
-        //After the 50th loop, this cycle will print null for the 51st magic spell;
-		    for (int i=1; i != 52; i++) 
-		    {
-			      System.out.println("Magic Spell Power #" + i + ": " + majiGame.pop());
-		    }
+      		//After the 50th loop, this cycle will print null for the 51st magic spell;
+		for (int i=1; i != 52; i++) 
+		{
+			System.out.println("Magic Spell Power #" + i + ": " + majiGame.pop());
+		}
 		
     }
 }
